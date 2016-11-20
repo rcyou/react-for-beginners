dabbf77c9eea9ead62bf574f30fc3aace8d708b5
Ryan Young <ryoung@mail.com>
2016-11-20 11:54:17 -0600

    project through video 7
    scaffold out the app; create Header component and start using props for
    dynamic content.

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,7 +1,9 @@
 // let's go!
 import React from 'react';
 import { render } from 'react-dom';
+import './css/style.css';
 
 import StorePicker from './components/StorePicker';
+import App from './components/App';
 
-render(<StorePicker/>, document.querySelector('#main'));
+render(<App/>, document.querySelector('#main'));
